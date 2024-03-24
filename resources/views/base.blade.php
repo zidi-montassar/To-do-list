884bfa880ffd16deffb4b3164e62b80537280ca6
Monta <zidimonta@hotmail.fr>
2024-03-24 03:05:33 +0100

    light backround added

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title') | To Do List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
+<body class="p-3 mb-2 bg-light text-dark">
 <nav class="navbar" style="background-color: #e3f2fd;">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('Task.index') }}">To Do List</a>
